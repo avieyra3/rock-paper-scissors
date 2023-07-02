b13009c93bec1ca483533416ecfedb3df4da7190
Alfonso <a.vieyra@hotmail.co.uk>
2023-07-02 10:58:14 -0700

    added game function. Program now runs for 5 rounds.

--- index.js
+++ index.js
@@ -10,23 +10,54 @@ function getComputeChoice() {
     else return 'scissors';
 }
 
-function playGame(userSelection, computerSelection) {
-    if (computerSelection === userSelection.toLowerCase())
-        return "tie";
+function playRound() {
+    let computerSelection = getComputeChoice();
+    console.log("AI has selected their weapon!")
+
+    let userSelection = prompt("Pick you weapon: Rock, Paper or Scissors");
+
+    if (computerSelection === userSelection.toLowerCase()){
+        console.log("This round is a draw!")
+        return 0;
+    }
     if (computerSelection == 'rock' && userSelection == 'scissor' || 
         computerSelection == 'scissors' && userSelection == 'paper' || 
-        computerSelection == 'paper' && userSelection == 'rock') 
-        return computerSelection + " beats " + userSelection + ". AI wins!";
-    else 
-        return userSelection + " beats " + computerSelection + ". You won!";
+        computerSelection == 'paper' && userSelection == 'rock') {
+        console.log(computerSelection + " beats " + userSelection)
+        return 1;
+    }
+    else {
+        console.log(userSelection + " beats " + computerSelection)
+        return 2;
+    }
+        
 }
 
-let computerSelection = getComputeChoice();
-console.log("AI has selected their weapon!")
+function game() {
+    let userScore = 0
+    let computerScore = 0
 
-let userSelection = prompt("Pick you weapon: Rock, Paper or Scissors");
-console.log("User has selected their weapon!")
+    for (let i = 0; i < 5; ++i) {
+        let result = playRound();
+        if (result == 0) {
+            continue;
+        }
+        if (result == 2) {
+            userScore += 1;
+            console.log("You won round " + (i + 1));
+        }
+        else {
+            computerScore += 1;
+            console.log("AI wins round " + (i + 1));
+        }
+        
+        console.log("User score: " + userScore)
+        console.log("computer score: " + computerScore)
+    }
 
-let result = playGame(userSelection, computerSelection);
+    if (userScore == computerScore) console.log("There are no winners in this game!")
+    else if (userScore > computerScore) console.log("You win!")
+    else console.log("You lose!")
+}
 
-console.log(result)
+game();
